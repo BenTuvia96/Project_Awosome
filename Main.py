921645092a7bf0965917b8cae535c0c4ca3e78fe
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-08-15 14:55:19 +0300

    basic sql logic

--- Main.py
+++ Main.py
@@ -1,14 +1,64 @@
-print ("hello world")
-def fib(n):
-    a,b=0,1
-    while a<n:
-        print (a,end=' ')
-        a,b=b,a+b
-    print()
-    def fib2(n):
-        result=[]
-        a,b=0,1
-        while a<n:
-            result.append(a)
-            a,b=b,a+b
-        return result
+import mysql.connector
+from mysql.connector import Error
+
+class DatabaseManager:
+    def __init__(self, host, user, password, database):
+        self.connection = self.connect(host, user, password, database)
+
+    def connect(self, host, user, password, database):
+        """Establish connection to the MySQL database."""
+        try:
+            connection = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database
+            )
+            if connection.is_connected():
+                print("Successfully connected to the database")
+                return connection
+        except Error as e:
+            print(f"Error while connecting to MySQL: {e}")
+            return None
+
+    def close(self):
+        """Close the database connection."""
+        if self.connection.is_connected():
+            self.connection.close()
+            print("MySQL connection is closed")
+
+    def execute_query(self, query, params=None):
+        """Execute a single query."""
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query, params)
+            self.connection.commit()
+        except Error as e:
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+
+    def fetch_all(self, query, params=None):
+        """Fetch all results of a query."""
+        cursor = self.connection.cursor()
+        cursor.execute(query, params)
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+# Usage:
+if __name__ == "__main__":
+    # These should ideally come from a config file or environment variables.
+    HOST = 'localhost'
+    USER = 'root'
+    PASSWORD = 'your_password'
+    DATABASE = 'your_database'
+
+    db_manager = DatabaseManager(HOST, USER, PASSWORD, DATABASE)
+
+    # Test fetching some data
+    records = db_manager.fetch_all("SELECT * FROM some_table")
+    for row in records:
+        print(row)
+
+    db_manager.close()
